96144af4f7875d91f472b33d31cf195b8723bdfc
Law.guancong <1491910100@qq.com>
2017-04-12 20:45:26 +0800

    My test!

--- react/photowall3333/component/photowall.js
+++ react/photowall3333/component/photowall.js
@@ -2,15 +2,31 @@
  * Created by Law.guancong on 2017/4/8.
  */
 //jsx ����
-
+var Picture = React.createClass({
+    render : function () {
+        console.log("Picture")
+        return (
+            <div style={this.props.style} onClick={this.props.handleClick}>
+                <img src={"img/" + this.props.value.fileName} alt=""/>
+                <br/>
+                {this.props.value.title}
+                <div>
+                    {this.props.value.desc}
+                </div>
+            </div>
+        )
+    }
+})
 var Photowall = React.createClass({
     getInitialState: function () {
+        console.log("getInitialState")
+
         return {
             picture: [{
                 style: {
                     top: 0,
                     left: 0,
-                    transform : "rotate(10deg)"
+                    transform : "rotate(0deg)"
                 },
                 rotate: 0,
                 isCenter: false,
@@ -20,9 +36,10 @@ var Photowall = React.createClass({
         };
     },
     const: {
-        centerPos: {
+        center: {
             top: 0,
-            left: 0
+            left: 0,
+            transform : "rotate(0deg)"
         },
         xLeftMin: 0,
         xLeftMax: 0,
@@ -32,6 +49,8 @@ var Photowall = React.createClass({
         yMax: 0
     },
     componentDidMount: function () {
+        console.log("componentDidMount")
+
         var stageDOM = this.refs.stage,
             wStageDOM = stageDOM.offsetWidth,
             hStageDOM = stageDOM.offsetHeight,
@@ -44,7 +63,7 @@ var Photowall = React.createClass({
             hHalfImgFigureDOM = hImgFigureDOM / 2;
         //计算图片位置的取值范围
         this.const = {
-            centerPos: {
+            center: {
                 top: hHalfStageDOM - hHalfImgFigureDOM,
                 left: wHalfStageDOM - wHalfImgFigureDOM
             },
@@ -58,28 +77,31 @@ var Photowall = React.createClass({
         this.rearrange(0);
     },
     rearrange : function (index) {
+        console.log("rearrange")
         for (var i = 0; i < this.state.picture.length; i++) {
             if (i < this.state.picture.length / 2) {
                 this.state.picture[i].style = {
                     top: Random(this.const.yMin, this.const.yMax),
-                    left: Random(this.const.xLeftMin, this.const.xLeftMax)
+                    left: Random(this.const.xLeftMin, this.const.xLeftMax),
+                    transform :  "rotate("+ Random(-30,30)+"deg)"
+
                 };
             } else {
                 this.state.picture[i].style = {
                     top: Random(this.const.yMin, this.const.yMax),
-                    left: Random(this.const.xRightMin, this.const.xRightMax)
+                    left: Random(this.const.xRightMin, this.const.xRightMax),
+                    transform :  "rotate("+ Random(-30,30)+"deg)"
                 };
             }
-            this.state.picture[i].style.transform =  "rotate("+ Random(-30,30)+"deg)"
             this.state.picture[i].isCenter = false;
             this.state.picture[i].isInverse = false;
         }
-        this.state.picture[index].style = this.const.centerPos;
-        this.state.picture[index].style.transform =  "rotate(0deg)"
+        this.state.picture[index].style = this.const.center;
         this.state.picture[index].isCenter = true;
         this.setState()// picture : this.state.picture
     },
     handleClick : function (index) {
+        console.log("handleClick")
         return function () {
             if(this.state.picture[index].isCenter){
                 // console.log(this.state.picture[index].isCenter)
@@ -100,6 +122,8 @@ var Photowall = React.createClass({
     },
 
     render : function () {
+        console.log(111)
+
         var pictureArr = []
         var radiusArr = []
         imgData.forEach(function (value,index) {
@@ -108,22 +132,15 @@ var Photowall = React.createClass({
                     style : {
                         top : 0,
                         left : 0,
-                        transform : "rotate(10deg)"
+                        transform : "rotate(0deg)"
                     },
                     rotate : 0,
                     isCenter : false,
                     isInverse : false
                 }
             }
-            pictureArr.push(<div key={index} onClick={this.handleClick(index)} ref="imgFigure" style={this.state.picture[index].style}>
-                <img src={"img/" + (index+1) + ".jpg"} alt=""/>
-                <div>
-                    {value.title} <br/>
-                    <div className="imageBack">
-                        {value.desc}
-                    </div>
-                </div>
-            </div>)
+            pictureArr.push(<Picture value={value} key={index} handleClick={this.handleClick(index)} ref="imgFigure"
+                                     style={this.state.picture[index].style}/>)
         }.bind(this))
         // console.log(this.state.picture)
         return (
